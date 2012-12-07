7114769bcb1fb3d829692a5d1d6072b423783d21
spline <spline@spline.com>
2012-12-07 04:24:17 -0500

    Initial push.

---
+++ test.py
@@ -0,0 +1,14 @@
+###
+# Copyright (c) 2012, spline
+# All rights reserved.
+#
+#
+###
+
+from supybot.test import *
+
+class WikipediaTestCase(PluginTestCase):
+    plugins = ('Wikipedia',)
+
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
