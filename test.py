de49c8f253061622865559ccfaa5898fddf7fd19
spline <reticulatingspline@github.com>
2014-10-11 09:52:51 -0400

    Add in tests.

--- test.py
+++ test.py
@@ -10,5 +10,10 @@ from supybot.test import *
 class WikipediaTestCase(PluginTestCase):
     plugins = ('Wikipedia',)
 
+    def testWikipedia(self):
+        conf.supybot.plugins.Wikipedia.disableANSI.setValue('True')
+        self.assertRegexp('wikipedia IRC', 'Internet Relay Chat :: Internet Relay Chat')
+        self.assertRegexp('wikisearch IRC', 'Search results for IRC :: Internet Relay Chat \| IRCAM')
+
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
